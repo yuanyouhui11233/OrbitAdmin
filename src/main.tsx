39b6402bbedb07a9f360094fa667b6d555928994
刘畅 <10646242+liu-chang403@user.noreply.gitee.com>
2025-02-09 13:12:26 +0800

    安装tailwind、styled-components、antd、zustand依赖

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-
-createRoot(document.getElementById('root')!).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+// tailwind css
+import "./theme/index.css";
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);

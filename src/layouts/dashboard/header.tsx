8b6edc20da582f6f93135773f14c21d06ce2504e
liuchang <1272029991@qq.com>
2025-05-29 17:58:36 +0800

    wip:header组件添加部分icon按钮

--- src/layouts/dashboard/header.tsx
+++ src/layouts/dashboard/header.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 import { useSettings } from "@/store/settingStore";
 import { HEADER_HEIGHT, OFFSET_HEADER_HEIGHT } from "./config";
-import IconButton from "@/components/icon/icon-button";
+import { IconButton, Iconify } from "@/components/icon/index";
 import Logo from "@/components/logo";
 import { ThemeLayout } from "#/enum";
 const Header = () => {
@@ -16,29 +16,38 @@ const Header = () => {
 					height: HEADER_HEIGHT,
 					transition: "height 200ms cubic-bezier(0.4, 0, 0.2, 1) 0ms",
 				}}
-				className="flex flex-grow items-center justify-between px-4 text-gray backdrop-blur xl:px-6 2xl:px-10"
+				className="flex flex-grow items-center justify-between px-4 backdrop-blur xl:px-6 2xl:px-10"
 			>
 				<div className="flex items-baseline">
-					{themeLayout === ThemeLayout.Vertical ? (
+					{themeLayout === ThemeLayout.Horizontal ? (
 						<IconButton
 							className="h-10 w-10 md:hidden"
 							onClick={() => setDrawerOpen(true)}
 						>
-							<Logo />
+							<Iconify icon="iconamoon:playlist-bold" size={24} />
 						</IconButton>
 					) : (
-						"logo"
+						<Logo />
 					)}
 					<div className="ml-4 hidden md:block">breadCrumb</div>
 				</div>
 				<div className="flex">
-					<div>1</div>
-					<div>1</div>
-					<div>1</div>
-					<div>1</div>
-					<button className="bg-[#4CAF50] hover:bg-purple" type="button">
-						btn
-					</button>
+					{/* searchBar */}
+					{/* 国际化 local */}
+
+					<IconButton>
+						<Iconify
+							icon="mdi:github"
+							size={24}
+							onClick={() => window.open("https://github.com")}
+						/>
+					</IconButton>
+					<IconButton>
+						<Iconify icon="carbon:logo-discord" size={24} />
+					</IconButton>
+					{/* notice */}
+					{/* setting */}
+					{/* accountDropdown */}
 				</div>
 			</div>
 		</header>

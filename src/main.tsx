b2cb150322e862d635ce332ccde46f76454e294a
liuchang <sorry00asd@Gmail.com>
2025-06-14 17:30:21 +0800

    fix: import virtual:svg-icons-register in main.ts for SVG icons

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 // tailwind css
 import "./theme/index.css";
+// svg icons
+import "virtual:svg-icons-register";
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<App />

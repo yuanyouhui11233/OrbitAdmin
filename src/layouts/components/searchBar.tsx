5be3ceccb988c9dc7e2f9effc1ba5bad2af8a554
liuchang <sorry00asd@Gmail.com>
2025-06-28 18:02:00 +0800

    feat: 添加@ant-design/v5-patch-for-react-19和react-use依赖，新增搜索栏组件

--- src/layouts/components/searchBar.tsx
+++ src/layouts/components/searchBar.tsx
@@ -1,14 +1,82 @@
-import { useMemo } from "react";
 import { IconButton, SvgIcon } from "@/components/icon/index";
+import { useBoolean } from "react-use";
+import { Modal, Empty, Input, type InputRef, Tag } from "antd";
+import { useRef, useState } from "react";
 export default function SearchBar() {
+	const [search, toggle] = useBoolean(false);
+	const inputRef = useRef<InputRef>(null);
+	const [searchQuery, setSearchQuery] = useState("");
+	const handleOpen = () => {
+		toggle(true);
+	};
+	const handleCancel = () => {
+		toggle(false);
+	};
 	return (
-		<div className="flex items-center">
-			<IconButton className="h-8 rounded-xl bg-hover py-2 text-xs font-bold">
-				<div className="flex">
-					<SvgIcon icon="ic-search" size="20" />
-					<span>Ctrl K</span>
-				</div>
-			</IconButton>
-		</div>
+		<>
+			<div className="flex items-center">
+				<IconButton
+					className="h-8 rounded-xl bg-hover py-2 text-xs font-bold"
+					onClick={handleOpen}
+				>
+					<div className="flex items-center">
+						<SvgIcon icon="ic-search" size="20" />
+						<span>Ctrl K</span>
+					</div>
+				</IconButton>
+			</div>
+
+			<Modal
+				open={search}
+				closeIcon={false}
+				onCancel={handleCancel}
+				styles={{
+					body: {
+						height: "400px",
+						display: "flex",
+						flexDirection: "column",
+						justifyContent: "center",
+					},
+				}}
+				title={
+					<Input
+						ref={inputRef}
+						value={searchQuery}
+						onChange={(e) => setSearchQuery(e.target.value)}
+						placeholder="Search..."
+						variant="borderless"
+						autoFocus
+						prefix={<SvgIcon icon="ic-search" size="20" />}
+						suffix={
+							<IconButton
+								className="h-6 rounded-md bg-hover text-xs font-bold"
+								onClick={handleCancel}
+							>
+								Esc
+							</IconButton>
+						}
+					/>
+				}
+				footer={
+					<div className="flex flex-wrap justify-center">
+						<div className="flex mr-4">
+							<Tag color="cyan">↑</Tag>
+							<Tag color="cyan">↓</Tag>
+							<span>to navigate</span>
+						</div>
+						<div className="flex mr-4">
+							<Tag color="cyan">↵</Tag>
+							<span>to select</span>
+						</div>
+						<div className="flex mr-4">
+							<Tag color="cyan">ESC</Tag>
+							<span>to close</span>
+						</div>
+					</div>
+				}
+			>
+				<Empty />
+			</Modal>
+		</>
 	);
 }
--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,9 @@ import App from "./App.tsx";
 import "./theme/index.css";
 // svg icons
 import "virtual:svg-icons-register";
+
+// antd React19兼容包 https://ant.design/docs/react/v5-for-19-cn
+import "@ant-design/v5-patch-for-react-19";
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<App />
